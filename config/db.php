b79f09611d49fb5d69c85b56b7c70bc3ebc20397
Vu32 <vdo_123@hotmail.com.au>
2025-03-21 14:38:38 +0800

    wip: Commence Framework
    - Create the db.php config file
    - Create helper.php file with functions

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Database Configuration File
+ *
+ * Filename:        db.php
+ * Location:        config/
+ * Project:         VD-SaaS-Vanilla-MVC-S1
+ * Date Created:    2025/03/21
+ *
+ * Author:          Vu Do J241918@tafe.wa.edu.au
+ *
+ */
+return [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'dbname' => 'vd_saas_fed_2025_s1',
+    'username' => 'vd_saas_fed_2025_s1',
+    'password' => 'Password1234'
+];
